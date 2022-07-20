c548ee8b5a4f22ede54a0211d82dd5641d67a197
Karin Heisterborg <karinheisterborg@gmail.com>
2022-07-19 22:21:04 -0300

    commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Navbar from './NavBar';
+
+function App() {
+  return (
+       <>
+          <header/>
+          <main>
+                <h2>Home</h2>
+          </main>
+          <footer>
+               <p>footer</p>
+          </footer>
+      </>
+  )
+}
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+          return (
+              <header id="header">
+                  <h1 className="titulo" >Mi Feria Home</h1>
+                  <nav>
+                      <a href="#">link</a>
+                      <a href="#">link</a>
+                  </nav>
+              </header>
+          )
+}
+      
+export default NavBar
